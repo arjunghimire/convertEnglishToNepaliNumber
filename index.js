a1ffecfa4758ddc1cef455045d97e1fab7008fbd
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-08-17 19:55:45 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function convertEnglishToNepaliNumber(value){
+  if (value === null || value === undefined) {
+    return 0;
+  }
+  const numbersInNepali = {
+    0: "०",
+    1: "१",
+    2: "२",
+    3: "३",
+    4: "४",
+    5: "५",
+    6: "६",
+    7: "७",
+    8: "८",
+    9: "९"
+  };
+  const valueToString = value.toString();
+  const arrayValue = valueToString.split("");
+  const normalizeArrayValue = arrayValue.map(value => {
+    return numbersInNepali[value];
+  });
+  return normalizeArrayValue.join("");
+};
+
+convertEnglishToNepaliNumber(120)   // १२०
+convertEnglishToNepaliNumber(1234)   // १२३४
+
